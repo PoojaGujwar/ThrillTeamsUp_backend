ae8b93166d667064c02abe2c449b5ea00341a215
unknown <gujwarpooja@gmail.com>
2025-01-15 15:45:32 +0530

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    fullname:{
+        firstname:{
+            type:String,
+            required:true
+        },
+        lastname:{
+            type:String
+        }
     },
     email:{
         type:String,
