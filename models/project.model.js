43aaef75d672090af8bf5ea0afaf28d68ec66c1a
unknown <gujwarpooja@gmail.com>
2025-02-01 05:35:15 +0530

    update in project

--- models/project.model.js
+++ models/project.model.js
@@ -4,10 +4,5 @@ const projectSchema = new mongoose.Schema({
     name:{ type: String, required: true, unique: true },
     description: { type: String },
     createdAt: { type: Date, default: Date.now },
-     team:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Team",
-            required:true
-        }
 })
 module.exports = mongoose.model('Project',projectSchema)
